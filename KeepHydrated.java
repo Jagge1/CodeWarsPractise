0c2a42f767dfc12dd4ffb6df4855c67bd9254012
Jagge1 <hellgrenjacob@hotmail.com>
2025-07-30 21:04:25 +0200

    A comment and a code snippet to check if a string starts with a certain letter

--- KeepHydrated.java
+++ KeepHydrated.java
@@ -1,6 +1,8 @@
 public class KeepHydrated  {
   public static int liters(double time)  {    
     double literSum = time * 0.5;
+
+    //Casting to int will round number
     return (int) literSum;
   }
 }
